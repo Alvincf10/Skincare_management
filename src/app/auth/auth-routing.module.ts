a13f5a58a684f9c10204887e719123f6a6b982be
Alvincf10 <alvinwijaya446@gmail.com>
2023-01-19 11:41:20 +0700

    update

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -4,6 +4,11 @@ import { AuthComponent } from './auth.component';
 
 const routes: Routes = [
   {
+    path: '',
+    redirectTo:'login',
+    pathMatch:'full'
+  },
+  {
     path: 'login',
     component:AuthComponent
   }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
+const API_ENDPOINT_PROD = 'https://be-beautybee.binus.space/';
+
 export const environment = {
-  production: true
+  production: true,
+  APIURL:API_ENDPOINT_PROD
 };
